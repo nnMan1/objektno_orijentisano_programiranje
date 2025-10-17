7c2184f79ccc5947cae1212e9c4c6b435367e9c0
Velibor Dosljak <dosljakvelibor@gmail.com>
2025-10-17 11:44:39 +0200

    Postavka zadataka za vjezbu

---
+++ Vjezbe/2025-2026/cas3/main.cpp
@@ -0,0 +1,15 @@
+
+/*
+* Napisati klasu za skup cijelih brojeva
+* - Konstruktor bez argumenata koji kreira prazan skup !!!!!!!!objasniti podrazumijevani konstruktor!!!!!!!! +
+* - Konstruktor kopije //inicijalizacija i dodjela +
+* - Konstruktor sa jednim argumentom, kreira skup sa jednim arguemntom (moze se koristiti za implicitnu konverziju) !!!!!!!!!!prvo implementirati njega!!!!!!!!!!!! +
+* - Destruktor. +
+* - Unija dva skupa. (vraca novi skup) //skup se predaje po referenci +
+* - Metoda dodaj(skup), ubacuje elemente skupa koji je predat u vec postojeci skup, i vraca referencu na skup nad kojim je pozvana metoda +
+* - Razlika skupova. (vraca novi skup) +
+* - Ukloni. (A.ukloni(B); uklanja elemente skupa B iz skupa A i vraca referencu na tako modifikovan skupa A) +
+* - Presjek skupova +
+* - ispis skupa na standardni izlaz +
+* - Metoda size(), vraca kardinalnost skupa +
+*/
